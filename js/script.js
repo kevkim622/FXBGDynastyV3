de9168d91fbea2e46ff1c64d392a958c28f93ac4
kevkim622 <101883432+kevkim622@users.noreply.github.com>
2023-09-09 18:19:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -67,3 +67,34 @@ const popoverTriggerList = document.querySelectorAll(
 const popoverList = [...popoverTriggerList].map(
   (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
 );
+
+new DataTable("#leagueRankings", {
+  paging: false,
+  order: [[3, "asc"]],
+});
+
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
